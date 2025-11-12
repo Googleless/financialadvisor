fe4d5f2c13527ab02d1a3b70a054fde4ca9636bf
Googleless <filipp.zemlyanskiy@mail.ru>
2025-11-12 17:55:36 +0300

    beginning to implement ai search capabilities

--- ai_handler.py
+++ ai_handler.py
@@ -1,9 +1,13 @@
 import os
+import json
 from openai import OpenAI
 from dotenv import load_dotenv
 from flask import session
 
 load_dotenv()
+
+with open("tools.json", "r", encoding="utf-8") as f:
+    tools = jso.load(f)
 
 api_keys = {
     'deepseek': os.environ.get('OPENAI_API_KEY'),
@@ -27,13 +31,13 @@ def run_deepseek(user_input, api_key):
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=[
-            {"role": "system", "content": "Ты — финансовый ассистент Землянского Филиппа Олеговича. Твоя задача — составить новостную сводку по новостям Сбербанка, Магнита и общих тенденций на рынке. Не давай финансовых советов."},
+            {"role": "system", "content": "Ты — финансовый ассистент Землянского Филиппа Олеговича. Твоя задача — составить подробный, но не длинный и затянутый, отчет о запрошенной теме с ссылками на источники. Ты будешь показывать только самую достоверную и актуальную информацию к запросу."},
             {"role": "user", "content": user_input},
         ],
-        temperature=1
+        temperature=1,
+        tools=tools
     )
     return response.choices[0].message.content
-
 
 def run_model():
     current_agent = session.get('ai_agent', 'deepseek')
